a1d82cb16645dcdb617e76a15b301f520372b0b3
Sai Kulkarni <saikulkarni0@gmail.com>
2021-12-02 20:11:50 -0500

    added CI, made test a template

---
+++ dn/test_solution.py
@@ -0,0 +1,40 @@
+
+import os
+def FNAME1(arr):    
+   return 0
+
+
+def FNAME2(arr):    
+   return 0
+
+def preprocess(fname):
+    fpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), fname)
+    with open(fpath) as f:
+        arr = f.readlines()
+
+    return arr
+
+def test_dayN(outfile=None):
+
+    test_arr = preprocess("test_input")
+    arr = preprocess("input")
+
+    if outfile:
+        log = open(outfile, 'a')
+        sys.stdout = log  
+        header = '## '
+        codeblock = '\n```'
+    else:
+        header = ''
+        codeblock = ''
+
+    print(header + "Day N Results:" + codeblock)
+
+    assert FNAME1(test_arr) == 0
+    print("P1:\t" + str(FNAME1(arr)))
+
+    assert FNAME2(test_arr) == 0
+    print("P2:\t" + str(FNAME2(arr)) + codeblock)
+
+if __name__ == "__main__":
+    test_dayN()
